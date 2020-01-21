73e4ba9ccc5be71ed20350266e49bd7cdb4829e8
v4vendeta <v4vendeta.lhy@outlook.com>
2020-01-21 13:19:21 +0800

    random sphere

--- main.cpp
+++ main.cpp
@@ -1,93 +1,104 @@
-#include <fstream>
-#include <string>
-#include <iostream>
+#include "svpng.inc"
+#include "sphere.h"
+#include "hittablelist.h"
+#include "camera.h"
+#include "random.h"
+#include "material.h"
+#include<cfloat>
 
-#include "float.h"
-#include "include/hittablelist.h"
-#include "include/sphere.h"
-#include "include/camera.h"
-#include "include/random.h"
-#include "include/material.h"
-
-
-//calculate color
 vec3 color(const ray& r, hittable *world, int depth) {
     hit_record rec;
-    if (world->hit(r, 0.001, FLT_MAX, rec)) {//ignore hits very near zero:
+    if (world->hit(r, 0.001, FLT_MAX, rec)) {
         ray scattered;
         vec3 attenuation;
-        if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {//reflect 50% of the light
+        if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
             return attenuation*color(scattered, world, depth+1);
         }
         else {
             return vec3(0,0,0);
         }
     }
-    else {
-        vec3 unit_direction = unit_vector(r.direction());
-        float t = 0.5*(unit_direction.y() + 1.0);
-        return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+    else{
+    vec3 unit_direction = unit_vector(r.direction());
+    float t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);   
     }
 }
 
 
-int main(){
+hittable *random_scene() {
+    int n = 500;
+    hittable **list = new hittable*[n+1];
+    list[0] =  new sphere(vec3(0,-1000,0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+    int i = 1;
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            float choose_mat = random_double();
+            vec3 center(a+0.9*random_double(),0.2,b+0.9*random_double());
+            if ((center-vec3(4,0.2,0)).length() > 0.9) {
+                if (choose_mat < 0.8) {  // diffuse
+                    list[i++] = new sphere(
+                        center, 0.2,
+                        new lambertian(vec3(random_double()*random_double(),
+                                            random_double()*random_double(),
+                                            random_double()*random_double()))
+                    );
+                }
+                else if (choose_mat < 0.95) { // metal
+                    list[i++] = new sphere(
+                        center, 0.2,
+                        new metal(vec3(0.5*(1 + random_double()),
+                                       0.5*(1 + random_double()),
+                                       0.5*(1 + random_double())),
+                                  0.5*random_double())
+                    );
+                }
+                else {  // glass
+                    list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+                }
+            }
+        }
+    }
 
-	std::cout<<"test1";
-	std::string path="test/spec.ppm";
-	std::ofstream file(path.c_str());
-	if(!file){
-		std::cerr<<"failed to create file ";
-		exit(-1);
-	}
+    list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+    list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+    list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
 
-	int nx=600; //width
-	int ny=360; //height
-	int ns=100; //multiple rays
+    return new hittable_list(list,i);
+}
 
-	file<<"P3\n"<<nx<<" "<<ny<<"\n255\n";
 
-/*
-	vec3 lower_left_corner(-2.0,-1.0,-1.0);
-	vec3 horizontal(4.0,0.0,0.0);
-	vec3 vertical(0.0,2.0,0.0);
-	vec3 origin(0.0,0.0,0.0);
-*/
-	hittable *list[4];
-    list[0] = new sphere(vec3(0,0,-1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
-    list[1] = new sphere(vec3(0,-100.5,-1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
-    list[2] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0.8, 0.6, 0.2)));
-    list[3] = new sphere(vec3(-1,0,-1), 0.5, new metal(vec3(0.8, 0.8, 0.8)));
-    hittable *world = new hittable_list(list,4);
-	camera cam;
-	for(int j=ny-1;j>=0;j--){ 
-		// up to down
-		for(int i=0;i<nx;i++){
-			//left to right
-			vec3 col(0, 0, 0);
+int main() {
 
-			//
-            for (int s = 0; s < ns; s++) {
-            	//let each ray hit the sphere
+    int nx=600,ny=300,ns=100;
+    unsigned char rgb[nx * ny * 3], *p = rgb;
+    FILE *fp = fopen("test.png", "wb");
+
+    vec3 lookfrom(0,13,0);
+    vec3 lookat(0,0,0);
+    float dist_to_focus=10;
+    float aperture=0.01;
+    camera cam(lookfrom, lookat, vec3(0,1,0), 20, float(nx)/float(ny), aperture, dist_to_focus);
+
+    hittable *world = random_scene();
+
+    for (int j = ny-1; j >= 0; j--){
+        for (int i = 0; i < nx; i++) {
+            vec3 col(0,0,0);
+            for(int s = 0; s < ns ; s++){
                 float u = float(i + random_double()) / float(nx);
                 float v = float(j + random_double()) / float(ny);
-                ray r = cam.get_ray(u, v);
-                col += color(r, world,0);//acumulate color
-            	}
-            //average the color
+                ray r = cam.get_ray(u,v);               
+                col += color(r, world, 0);           
+            }
             col /= float(ns);
-			col = vec3( sqrt(col[0]), sqrt(col[1]), sqrt(col[2]) );//gamma correction
-			
-			int ir=int(255.99*col[0]);
-			int ig=int(255.99*col[1]);
-			int ib=int(255.99*col[2]);
-			
-			file<<ir<<" "<<ig<<" "<<ib<<std::endl;
-		}
-	}
-
-
-
-	std::cout<<"test2";
-	return 0;
+            col = vec3( sqrt(col[0]), sqrt(col[1]), sqrt(col[2]) );
+            *p++ = int(255.99*col[0]);    /* R */
+            *p++ = int(255.99*col[1]);    /* G */
+            *p++ = int(255.99*col[2]);    /* B */   
+        }
+    }
+    svpng(fp, nx, ny, rgb, 0);
+    fclose(fp); 
+    return 0;
 }
--- ppmviewer.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import cv2
-path='test/spec.ppm'
-img = cv2.imread(path)
-
-cv2.namedWindow('image',cv2.WINDOW_NORMAL)
-cv2.imshow('image',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
