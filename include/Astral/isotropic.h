9a3683eab0864e216a16bee9d6e014d06bae3f32
Clang Robot <hy.lu@outlook.com>
2023-08-10 15:35:00 +0000

    :art: Committing clang-format changes

--- include/Astral/isotropic.h
+++ include/Astral/isotropic.h
@@ -1,19 +1,21 @@
 #ifndef ISOTROPIC
-#define ISOTROPIC 
+#define ISOTROPIC
 
 #include "material.h"
 
-class isotropic:public material{
+class isotropic : public material {
 public:
-	isotropic(texture *a) : albedo(a) {}
-	virtual bool scatter(const ray& r_in,const hit_record& rec,vec3& attenuation,ray& scatterd)const;
-	texture *albedo;
+  isotropic(texture *a) : albedo(a) {}
+  virtual bool scatter(const ray &r_in, const hit_record &rec,
+                       vec3 &attenuation, ray &scatterd) const;
+  texture *albedo;
 };
 
-bool isotropic::scatter(const ray& r_in,const hit_record& rec,vec3& attenuation,ray& scatterd)const{
-	scatterd=ray(rec.p,random_in_unit_sphere());
-	attenuation=albedo->value(rec.u,rec.v,rec.p);
-	return true;
+bool isotropic::scatter(const ray &r_in, const hit_record &rec,
+                        vec3 &attenuation, ray &scatterd) const {
+  scatterd = ray(rec.p, random_in_unit_sphere());
+  attenuation = albedo->value(rec.u, rec.v, rec.p);
+  return true;
 }
 
 #endif
