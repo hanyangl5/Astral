f25c729f5c5c0e42f4d9ec2fc6c59e79b4322241
Clang Robot <hy.lu@outlook.com>
2023-08-13 09:44:28 +0000

    :art: Committing clang-format changes

--- include/Astral/render.h
+++ include/Astral/render.h
@@ -1,7 +1,8 @@
-#include "vec3.h"
-#include "hittable.h"
 #include "camera.h"
+#include "hittable.h"
+#include "vec3.h"
 
 vec3 color(const ray &r, hittable *world, int depth);
 
-void render(int w, int h, int spp, unsigned char* color_buffer,hittable* scene, camera* cam);
+void render(int w, int h, int spp, unsigned char *color_buffer, hittable *scene,
+            camera *cam);
